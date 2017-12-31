31f6d5216ec8d994401199b25a9d4305a85f9834
chrisjamesr <chrisjames.r@gmail.com>
2017-12-31 13:16:05 -0500

    change user_name to name in helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    session[:user_name]
+    session[:name]
   end 
 
   def logged_in?
-    !!session[:user_name]
+    !!session[:name]
   end
 end
